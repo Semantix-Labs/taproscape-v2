ccc3972f072d38739ffb766ad0432f0c50770d62
herath98 <harshaudayanga401@gmail.com>
2024-10-10 20:33:17 +0530

    changes

--- components/WhatsAppChat.tsx
+++ components/WhatsAppChat.tsx
@@ -1,5 +1,5 @@
-import { FaWhatsapp } from 'react-icons/fa';
-
+import Link from 'next/link';
+import { IoLogoWhatsapp } from "react-icons/io";
 export default function WhatsAppChat() {
   const phoneNumber = '94777181236'; // International format, without + or spaces
   const message = 'Hello, I would like to know more!'; // Default message
@@ -8,14 +8,14 @@ export default function WhatsAppChat() {
 
   return (
     <div className="fixed bottom-4 right-4 z-50">
-      <a
+      <Link
         href={whatsappLink}
         target="_blank"
         rel="noopener noreferrer"
         className="hover:scale-110 transition-transform duration-200"
       >
-        <FaWhatsapp size={40} color="#25D366" className='hover:scale-120 animate-bounce transition-transform duration-200 coursor-pointer' />
-      </a>
+        <IoLogoWhatsapp  size={60} color="#25D366" className='hover:scale-120 animate-bounce transition-transform duration-200 coursor-pointer' />
+      </Link>
     </div>
   );
 }
